e6f1637dea80cfa39e24b52bfc98c2ca673cf0bc
Amirbek <124518356+Merkavacorporation@users.noreply.github.com>
2023-03-23 17:33:43 +0500

    Add files via upload
    Kalkulyator o'yini

---
+++ Game.js
@@ -0,0 +1,15 @@
+const Score1=document.getElementById("score1")
+  Score=0;
+  
+function Game1(){
+ Score1.innerHTML=Score+=100;
+}
+function Game2(){
+  Score1.innerHTML=Score-=100;
+  
+}
+function Finish1(){
+ alert("Thanks for play our game!.<Merkava Corp>") 
+  
+  
+}
